1df03be7396fd9290bdb018c2e2f2748db91be4f
MattYews <mhughes91@hotmail.co.uk>
2018-05-14 00:13:33 +0100

    Moved line

--- src/main/java/com/scenescrape/Main.java
+++ src/main/java/com/scenescrape/Main.java
@@ -21,15 +21,15 @@ public class Main {
     private static final String RECIPIENT = System.getenv("RECIPIENT");
 
     public String handleRequest(Map<String, Object> input, Context context) {
-        context.getLogger().log("Email sent successfully");
         scrape();
+        context.getLogger().log("Email sent successfully");
 
         return null;
     }
 
     private void scrape() {
         try {
-            Document doc = Jsoup.connect(URL).userAgent("Mozilla").get();
+            Document doc = Jsoup.connect(URL).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
 
             StringBuilder builder = new StringBuilder();
             builder.append("Here are the latest releases: \n\n\n");
